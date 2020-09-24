112983c2e9d4bf21f9b17b014639f8084a160bdf
gitliffi <muhdaliffi@gmail.com>
2020-09-25 01:22:48 +0800

    finalized changes

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const grid = document.getElementById("grid");
 const reset = document.getElementById ("reset-btn")
+const color = document.getElementById("color")
+const size = document.getElementById("size")
 
-function makeGrid(input) {
+function makeGrid() {
+    let input = prompt("How many cells would you like on each side?")
     grid.style.setProperty('--grid-rows', input)
     grid.style.setProperty('--grid-cols', input)
     for (i=0;i < (input*input);i++) {
         let cell = document.createElement('div');
         grid.appendChild(cell).className = "grid-cell";
+        cell.style.backgroundColor = "white";
         cell.addEventListener("mouseover", ()=> cell.style.backgroundColor = "black");
         reset.addEventListener("click",()=>cell.style.backgroundColor = "white");
     };
 };
 
-makeGrid(100);
+makeGrid();
+
+size.addEventListener("click", makeGrid);
